7879467045f5f10a6403da02e0a0ac4f2b83faeb
jairo <mail.jairo@gmail.com>
2016-09-23 21:14:05 -0700

    routing feedname properly.adding plugin to tinymce

--- src/components/newpost/newpost.ts
+++ src/components/newpost/newpost.ts
@@ -49,10 +49,23 @@ export class NewPostComponent implements OnInit {
             selector: '#editor',
             height: 200,
             plugins: [
-                'advlist autolink lists link image charmap print preview anchor',
+                'textpattern advlist autolink lists link image charmap print preview anchor',
                 'searchreplace visualblocks code fullscreen',
                 'insertdatetime media table contextmenu paste code'
             ],
+ textpattern_patterns: [
+     {start: '*', end: '*', format: 'italic'},
+     {start: '**', end: '**', format: 'bold'},
+     {start: '#', format: 'h1'},
+     {start: '##', format: 'h2'},
+     {start: '###', format: 'h3'},
+     {start: '####', format: 'h4'},
+     {start: '#####', format: 'h5'},
+     {start: '######', format: 'h6'},
+     {start: '1. ', cmd: 'InsertOrderedList'},
+     {start: '* ', cmd: 'InsertUnorderedList'},
+     {start: '- ', cmd: 'InsertUnorderedList'}
+  ],
             toolbar: 'insertfile undo redo | bold | bullist | link image',
             setup: (editor) => {
                 editor.on('change', (e) => {
