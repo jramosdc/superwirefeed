b01d0062d68f0ccef305f2a97e72a63783f89c7f
Yousuf Qutubuddin <usuf52@gmail.com>
2016-12-08 13:24:54 +0500

    pdf view

--- src/components/viewpost/viewpost.ts
+++ src/components/viewpost/viewpost.ts
@@ -29,7 +29,7 @@ export class ViewPostComponent {
             if (this.postid) {
                 this.as.loadPost(this.postid).subscribe((post) => {
                     this.post = post;
-                    this.post['purl'] = sanitizer.bypassSecurityTrustResourceUrl(post.pdfLink);
+                    this.post['purl'] = post.pdfLink ? sanitizer.bypassSecurityTrustResourceUrl((post.pdfLink).replace("http:", "")) : post.pdfLink;
                     this.post['gurl'] = sanitizer.bypassSecurityTrustResourceUrl(post.gsheetLink);
                     setTimeout(() => {
                         $('.linkify')['linkify']();
