4cf2c59aa7ca9a141f9d53e0addb5147a6990ab7
nikoloza <nika.tomadze@gmail.com>
2017-01-29 16:51:42 +0400

    added uikit codes

--- src/components/uikit/uikit.ts
+++ src/components/uikit/uikit.ts
@@ -1,7 +1,12 @@
 'use strict'
 
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+@NgModule({
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+})
 
 @Component({
   host: {},
