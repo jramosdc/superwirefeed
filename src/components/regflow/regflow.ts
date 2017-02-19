5c369d7782d5061c37545c464dbaca8feb50ff77
nikoloza <nika.tomadze@gmail.com>
2017-02-19 18:17:55 +0400

    navigation of modal

--- src/components/regflow/regflow.ts
+++ src/components/regflow/regflow.ts
@@ -9,10 +9,27 @@ declare var $:JQueryStatic;
 })
 
 export class RegFlow {
+  Step: number;
+  Steps: string[];
+
   constructor () {
+    this.Step = 0;
+    this.Steps = ['welcome', '']
   }
 
   ngOnInit() {
-    $('select')['material_select']()
+    $('form select')['material_select']()
+  }
+
+  onSubmit(d) {
+    console.log(d)
+    if (d.valid) this.Step += 1;
+    $('form select')['material_select']()
+
+    console.log(this.Step)
+  }
+
+  onDot(step) {
+    this.Step = step;
   }
 }
