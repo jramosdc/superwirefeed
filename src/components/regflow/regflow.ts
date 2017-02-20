6899de85cc4e020b5c28fe7cc6bb847c958c46d4
nikoloza <nika.tomadze@gmail.com>
2017-02-20 05:30:21 +0400

    updated slider effects for modal

--- src/components/regflow/regflow.ts
+++ src/components/regflow/regflow.ts
@@ -1,11 +1,37 @@
 'use strict'
 
-import { Component } from '@angular/core';
+import { Component, trigger, transition, style, animate } from '@angular/core';
 declare var $:JQueryStatic;
 
 @Component({
   host: {},
-  template: require('./regflow.html')
+  template: require('./regflow.html'),
+  animations: [
+    trigger(
+      'modalSlider', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)', opacity: 0}),
+          animate('200ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('200ms', style({transform: 'translateX(-100%)', opacity: 0}))
+        ])
+      ]
+    ),
+    trigger(
+      'modalFader', [
+        transition(':enter', [
+          style({opacity: 0}),
+          animate('200ms', style({opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({opacity: 1}),
+          animate('200ms', style({opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 
 export class RegFlow {
