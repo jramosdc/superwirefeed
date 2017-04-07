ab492b23d253dc9d7bea05638506877cd81ef865
nikoloza <nika.tomadze@gmail.com>
2017-04-07 21:04:53 +0400

    updated dist

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/clipboard/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/moment/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
