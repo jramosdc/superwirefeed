e1ddbc6bbec76625f20a564a33d97f40601112a2
Muhammad Kamran <smkamranqadri@yahoo.com>
2017-05-17 19:38:45 +0500

    fix editor

--- src/components/editpost/editpost.ts
+++ src/components/editpost/editpost.ts
@@ -163,45 +163,45 @@ export class EditPostComponent implements OnInit {
     }
 
     private viewInitialize() {
-        $('select')['material_select']();
-        tinymce.remove();
-        tinymce.init({
-            selector: '#editor',
-            height: 200,
-            menubar: false,
-            statusbar: false,
-            plugins: [
-                'textpattern advlist autolink lists link image charmap print preview anchor',
-                'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table contextmenu paste code'
-            ],
-            content_css: [
-                '//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i',
-                '//www.mucholab.net/css/tinymce.css'],
-            textpattern_patterns: [
-                { start: '*', end: '*', format: 'italic' },
-                { start: '**', end: '**', format: 'bold' },
-                { start: '#', format: 'h1' },
-                { start: '##', format: 'h2' },
-                { start: '###', format: 'h3' },
-                { start: '####', format: 'h4' },
-                { start: '#####', format: 'h5' },
-                { start: '######', format: 'h6' },
-                { start: '1. ', cmd: 'InsertOrderedList' },
-                { start: '* ', cmd: 'InsertUnorderedList' },
-                { start: '- ', cmd: 'InsertUnorderedList' }
-            ],
-            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image code',
-            setup: (editor) => {
-                editor.on('change', (e) => {
-                    this.post['detail'] = editor.getContent();
-                });
-                editor.on('init', (e) => {
-                    console.log('tiny init', this.post['detail']);
-                    tinymce.activeEditor.setContent(this.post['detail']);
-                });
-            }
-        });
+        // $('select')['material_select']();
+        // tinymce.remove();
+        // tinymce.init({
+        //     selector: '#editor',
+        //     height: 200,
+        //     menubar: false,
+        //     statusbar: false,
+        //     plugins: [
+        //         'textpattern advlist autolink lists link image charmap print preview anchor',
+        //         'searchreplace visualblocks code fullscreen',
+        //         'insertdatetime media table contextmenu paste code'
+        //     ],
+        //     content_css: [
+        //         '//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i',
+        //         '//www.mucholab.net/css/tinymce.css'],
+        //     textpattern_patterns: [
+        //         { start: '*', end: '*', format: 'italic' },
+        //         { start: '**', end: '**', format: 'bold' },
+        //         { start: '#', format: 'h1' },
+        //         { start: '##', format: 'h2' },
+        //         { start: '###', format: 'h3' },
+        //         { start: '####', format: 'h4' },
+        //         { start: '#####', format: 'h5' },
+        //         { start: '######', format: 'h6' },
+        //         { start: '1. ', cmd: 'InsertOrderedList' },
+        //         { start: '* ', cmd: 'InsertUnorderedList' },
+        //         { start: '- ', cmd: 'InsertUnorderedList' }
+        //     ],
+        //     toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image code',
+        //     setup: (editor) => {
+        //         editor.on('change', (e) => {
+        //             this.post['detail'] = editor.getContent();
+        //         });
+        //         editor.on('init', (e) => {
+        //             console.log('tiny init', this.post['detail']);
+        //             tinymce.activeEditor.setContent(this.post['detail']);
+        //         });
+        //     }
+        // });
         console.log('ng init');
     }
 
